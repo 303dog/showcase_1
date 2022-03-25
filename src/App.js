7a2a27b6c231e6bd6cb94d776faabb30ec2da0e3
303dog <303dog@gmail.com>
2022-03-25 15:54:02 -0600

    add login comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 import './App.css';
 import Dashboard from './Components/Dasboard';
 import Login from './Components/Login';
@@ -7,33 +7,12 @@ import Preferences from './Components/Preferences';
 import Upload from './Components/Upload';
 
 function App() {
-  const [token, setToken] = useState();
-  
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+ 
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <header className="App-header">
-      <BrowserRouter>
-        <Routes>      
-          <Route path='/login'>
-            <Login/>
-          </Route>
-          <Route path='/dashboard'>
-            <Dashboard/>
-          </Route>
-          <Route path='/preferences'>
-            <Preferences/>
-          </Route>
-          <Route path='/upload'>
-            <Upload/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      </header>
-    </div>
+    <main className="wrapper">
+      <h1>App.js</h1>
+      <Login/>
+    </main>
   );
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import './Login.css'
+import { useRef, useEffect, useState } from 'react';
+import './Login.css';
 
-export default function Login() {
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
   return(
-    <div className="login-wrapper">
-    <h1>Hello [name], Please Log In</h1>
-    <form>
-      <label>
-        <p>Admin-username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-    </div>
+      <>
+        {success ? (
+            <section>
+                <h1> You are logged in!</h1>
+                <br />
+                <p>
+                    <a href='#'>Go Home</a>
+                </p>
+            </section>
+        ) : (
+      
+    <section>
+        <p ref={errRef} className={errMsg ? 'errmsg' :
+        "offscreen"} aria-live='assertive'>{errMsg}</p>
+        <h1>Log in</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username</label>
+            <input 
+                type='text' 
+                id='username'
+                ref={userRef}
+                autoComplete='off'
+                onChange={(e) => setUser(e.target.value)}
+                value={user} 
+                required
+                />
+            <label htmlFor='password'>Password</label>
+            <input 
+                type='password' 
+                id='password'
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd} 
+                required
+                />
+                <button>Sign in</button>
+        </form>
+        <p>
+            Want to create an account?
+            <span className='line'>
+                {/*put router link here*/}
+                <a href='#'>Create new</a>
+            </span>
+        </p>
+    </section>
+        )}
+        </>
+
   )
 }
+
+export default Login
