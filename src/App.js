b7527ff02ed2fec0f3fe46ca6346e95781c53829
303dog <303dog@gmail.com>
2022-03-31 08:07:32 -0600

    working on forms-dashboard

--- src/App.js
+++ src/App.js
@@ -7,32 +7,18 @@ import Login from './Components/Login';
 import Preferences from './Components/Preferences';
 import Upload from './Components/Upload';
 import CreateUser from './Components/CreateUser';
-import CounterWrapper from './Components/CounterWrapper';
+
+
 function App() {
  
   return (
     <main className="wrapper">
       <h1>App.js</h1>
-      <CounterWrapper
-    render={({ count, increment, decrement }) => (
-      <div>
-        <h1>Render Props Counter Example</h1>
-        <div>
-          <p>{count}</p>
-          <button type="button" onClick={() => increment()}>
-            Increment
-          </button>
-          <button type="button" onClick={() => decrement()}>
-            Decrement
-          </button>
-        </div>
-      </div>
-    )}
-  />
+    
+      <Dashboard/>
 
       <CreateUser />
       {/*<Login/>
-      <Dashboard/>
       <Preferences/>
       <Upload/>*/}
     </main>
--- src/Components/Dasboard.js
+++ src/Components/Dasboard.js
@@ -1,12 +1,43 @@
 import React from 'react';
 
 
-export default function Dashboard() {
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    return(
-        <h2>
-            Login page | Dashboard
-        </h2>
-    )
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="name" value={this.state.value.name} onChange={this.handleChange} />
+        </label>
+        
+          <li type='item' value={this.state.value} onChange={this.handleChange}>item:</li>
+          <li>item:</li>
+        
+        <input type="submit" value="Submit" />
+        <div>{this.state.value}</div>
+      </form>
+    );
+  }
 }
+
+export default Dashboard
